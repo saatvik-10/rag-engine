5d0460e850c7453eff67563812cfce2edd402ae6
saatvik-10 <saatvikmadan529@gmail.com>
2026-06-23 18:33:57 +0530

    added chunking service

---
+++ app/services/chunking_service.py
@@ -0,0 +1,17 @@
+def chunk_embeddings(text: str, chunk_size: int = 500, overlap: int = 100):
+    words = text.split()
+
+    chunks = []
+
+    start = 0
+
+    while start < len(words):
+        end = start + chunk_size
+
+        chunk = " ".join(words[start:end])
+
+        chunks.append(chunk)
+
+        start += chunk_size - overlap
+
+    return chunk
