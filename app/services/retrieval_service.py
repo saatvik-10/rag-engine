8fcf67d7b0f33f71e4636d58b9af71d2c9c6e0a3
saatvik-10 <saatvikmadan529@gmail.com>
2026-06-25 15:46:42 +0530

    updated retrieval service

--- app/services/retrieval_service.py
+++ app/services/retrieval_service.py
@@ -5,17 +5,17 @@ from sqlalchemy.orm import Session
 
 
 def chunks_retrieval(query: str, top_k: int, db: Session):
-    query_embeddings = generate_embeddings(query)
+    query_embedding = generate_embeddings(query)
 
     chunks = db.query(Chunk).all()
 
     results = []
 
     for chunk in chunks:
-        score = cosine_similarity(query_embeddings, chunk.embedding)
+        score = cosine_similarity(query_embedding, chunk.embedding)
 
         results.append({"id": chunk.id, "chunk": chunk.text, "score": score})
 
-        results.sort(key=lambda x: x["score"], reverse=True)
+    results.sort(key=lambda x: x["score"], reverse=True)
 
-    return results[::top_k]
+    return results[:top_k]
