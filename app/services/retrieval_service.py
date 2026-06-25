0679b1ae4c91aed79bffaa443570366bae11298e
saatvik-10 <saatvikmadan529@gmail.com>
2026-06-25 15:41:54 +0530

    added retrieval service

---
+++ app/services/retrieval_service.py
@@ -0,0 +1,21 @@
+from app.models.chunk import Chunk
+from app.services.similarity_service import cosine_similarity
+from app.services.embedding_service import generate_embeddings
+from sqlalchemy.orm import Session
+
+
+def chunks_retrieval(query: str, top_k: int, db: Session):
+    query_embeddings = generate_embeddings(query)
+
+    chunks = db.query(Chunk).all()
+
+    results = []
+
+    for chunk in chunks:
+        score = cosine_similarity(query_embeddings, chunk.embedding)
+
+        results.append({"id": chunk.id, "chunk": chunk.text, "score": score})
+
+        results.sort(key=lambda x: x["score"], reverse=True)
+
+    return results[::top_k]
