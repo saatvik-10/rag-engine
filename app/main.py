daa30c20465274531b26ed9ee2f1e94ff760d933
saatvik-10 <saatvikmadan529@gmail.com>
2026-06-25 16:29:37 +0530

    added ingestion route

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
-from fastapi import FastAPI
-from app.db.db import Base, engine
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
 
-Base.metadata.create_all(bind=engine)
+from app.services.ingestion_service import ingest_document
+
+from app.schemas.ingest_schema import IngestRequest
+
+from app.db.db import get_db
 
 app = FastAPI()
+
+
+@app.post("/ingest")
+def ingest_create(req: IngestRequest, db: Session = Depends(get_db)):
+    return ingest_document(req.text, db)
