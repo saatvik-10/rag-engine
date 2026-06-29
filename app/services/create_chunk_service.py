f75a215bb3482871d1d6b1e7defac53954201dd3
saatvik-10 <saatvikmadan529@gmail.com>
2026-06-29 20:31:44 +0530

    added table columns

--- app/services/create_chunk_service.py
+++ app/services/create_chunk_service.py
@@ -5,7 +5,12 @@ from app.models.chunk import Chunk
 
 def create(chunk_data: Chunk, db: Session):
     stored_chunk = Chunk(
-        id=str(uuid4()), text=chunk_data.text, embedding=chunk_data.embedding
+        id=str(uuid4()),
+        text=chunk_data.text,
+        embedding=chunk_data.embedding,
+        page=chunk_data.page,
+        chunk_index=chunk_data.chunk_index,
+        source=chunk_data.source,
     )
 
     db.add(stored_chunk)
