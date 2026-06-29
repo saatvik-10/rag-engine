feee62e14d20e14d56db544f75e0d4fa1c378a40
saatvik-10 <saatvikmadan529@gmail.com>
2026-06-29 20:31:18 +0530

    updated ingestion route

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
-from fastapi import Depends, FastAPI
+import os
+import shutil
+
+from fastapi import Depends, FastAPI, UploadFile, File
 from sqlalchemy.orm import Session
 
 from app.services.ingestion_service import ingest_document
@@ -11,10 +14,22 @@ from app.db.db import get_db
 
 app = FastAPI()
 
+UPLOAD_DIR = "uploads"
+
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 @app.post("/ingest")
-def ingest_create(req: IngestRequest, db: Session = Depends(get_db)):
-    return ingest_document(req.text, db)
+def ingest_create(file: UploadFile = File(...), db: Session = Depends(get_db)):
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+    
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    
+    ingest_document(file_path, db)
+    
+    return {
+    "message": "Document ingested successfully."
+    }
 
 
 @app.post("/search")
