05c0dbf782ca647afc1a1c35cbc5fe1b9c99a8d1
saatvik-10 <saatvikmadan529@gmail.com>
2026-07-04 20:55:10 +0530

    added context builder service

---
+++ app/services/context_builder_service.py
@@ -0,0 +1,18 @@
+def context_builder(results: list[dict]) -> str:
+    res_chunk = []
+
+    for result in results:
+        source = result["source"]
+        page = result["page_number"]
+        chunk = result["text"]
+
+        formatted_schunk = f"""Document::{source}
+                            Page: {page}
+                            {chunk}
+                            """
+
+        res_chunk.append(formatted_schunk)
+
+        context = "\n\n----------------------------------------\n\n".join(res_chunk)
+
+    return context
