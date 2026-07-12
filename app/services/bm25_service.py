aebb6fe7e84e398d9b8ae5b66884fd0fd717b944
saatvik-10 <saatvikmadan529@gmail.com>
2026-07-12 22:00:19 +0530

    feat: implemented bm25 from scratch

--- app/services/bm25_service.py
+++ app/services/bm25_service.py
@@ -1,14 +1,69 @@
+import math
+
 from sqlalchemy.orm import Session
 
 from app.models.chunk import Chunk
 
+from app.config.bm25_config import K1, B
+
 
 def bm25_retrieval(query: str, top_k: int, db: Session):
-    document_frequency = {}
+    results = []
+
+    idf = {}
 
     chunks = db.query(Chunk).all()
 
     query_tokens = set(query.lower().split())
+
+    average_document_length = compute_average_doc_length(chunks)
+
+    document_frequency = compute_document_frequency(chunks, query_tokens)
+
+    for token in query_tokens:
+        idf[token] = compute_idf(document_frequency[token], len(chunks))
+
+    for chunk in chunks:
+        words = chunk.text.lower().split()
+        score = 0
+
+        for token in query_tokens:
+            term_frequency = compute_tf(words, token)
+
+            score += compute_bm25(
+                term_frequency,
+                idf[token],
+                len(words),
+                average_document_length,
+            )
+
+        results.append(
+            {
+                "id": chunk.id,
+                "text": chunk.text,
+                "page_number": chunk.page,
+                "chunk_index": chunk.chunk_index,
+                "source": chunk.source,
+                "score": score,
+            }
+        )
+
+    results.sort(key=lambda x: x["score"], reverse=True)
+
+    return results[:top_k]
+
+
+def compute_average_doc_length(chunks: list[Chunk]) -> float:
+    total_words = 0
+
+    for chunk in chunks:
+        total_words += len(chunk.text.lower().split())
+
+    return total_words / len(chunks)
+
+
+def compute_document_frequency(chunks, query_tokens) -> dict:
+    document_frequency = {}
 
     for token in query_tokens:
         cnt = 0
@@ -20,4 +75,28 @@ def bm25_retrieval(query: str, top_k: int, db: Session):
 
         document_frequency[token] = cnt
 
-    print(document_frequency)
+    return document_frequency
+
+
+def compute_idf(document_frequency: int, total_documents: int) -> float:
+    return math.log(
+        (total_documents - document_frequency + 0.5) / (document_frequency + 0.5)
+    )
+
+
+def compute_tf(words: list[str], token: str) -> int:
+    return words.count(token)
+
+
+def compute_bm25(
+    term_frequency: int,
+    idf: float,
+    document_length: int,
+    average_document_length: float,
+) -> float:
+    document_norm = 1 - B + B * (document_length / average_document_length)
+
+    N = term_frequency * (K1 + 1)
+    D = term_frequency + K1 * document_norm
+
+    return idf * (N / D)
